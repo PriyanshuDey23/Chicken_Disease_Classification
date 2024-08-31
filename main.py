56c1ad7f5ee9075283afa3585254207f75385b74
PriyanshuDey23 <priyanshudey.ds@gmail.com>
2024-08-31 16:53:44 +0530

    Training Added

--- main.py
+++ main.py
@@ -39,3 +39,27 @@ except Exception as e:
 
 
 
+
+# src\Chicken_Disease_Classification\pipeline\stage_03_training.py
+
+from Chicken_Disease_Classification.pipeline.stage_03_training import ModelTrainingPipeline
+
+STAGE_NAME="Training Stage"
+try:
+    logger.info(f"Stage {STAGE_NAME} Started")
+    obj=ModelTrainingPipeline()  # Calling the class
+    obj.main()                           # Calling the main method,Start the data ingestion part
+    logger.info(f" Stage {STAGE_NAME} Completed")
+
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+# After executing we will  artifacts\training
+
+
+
+
+
+
